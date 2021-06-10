9c6c17cc419aaf3d0c770028014573d2fef2eaa1
cgnnanna <cgnnanna@gmail.com>
2021-06-10 13:49:38 +0100

    modified .then to await promises in the fetch fare function of rate controller

--- controller/rateController.js
+++ controller/rateController.js
@@ -12,40 +12,67 @@ const getGeolocation = async (address) => {
 retrieves the amount for a pick-up and delivery services from gokada logistics
 */
 const fetchFare = async (req, res) => {
-    console.log(req.body);
-    const deliveryAddress = await getGeolocation(req.body.deliveryAddress);
-    const pickupAddress = await getGeolocation(req.body.pickupAddress);
-    const delivery = deliveryAddress.results[0].geometry.location;
-    const pickup = pickupAddress.results[0].geometry.location;
-    fetch("https://private-10941a-gokada2.apiary-mock.com/api/developer/order_estimate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: {
+    try {
+        console.log("fetching fare for...", req.body);
+        const deliveryAddress = await getGeolocation(req.body.deliveryAddress);
+        const pickupAddress = await getGeolocation(req.body.pickupAddress);
+        const delivery = deliveryAddress.results[0].geometry.location;
+        const pickup = pickupAddress.results[0].geometry.location;
+        const url = process.env.GOKADA_API_MOCK;
+        const body = {
             api_key: process.env.GOKADA_API_KEY,
             pickup_latitude: pickup.lat.toString(),
             pickup_longitude: pickup.lng.toString(),
             delivery_latitude: delivery.lat.toString(),
             delivery_longitude: delivery.lng.toString()
-        }
-    })
-    .then(response=>{
-        return response.json();
-    })
-    .then(body=>{
-        return res.json({
-            success: true,
-            data: body
+        };
+        console.log(`HTTP request to ${url} with body`, body);
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: body
         });
-    })
-    .catch(err=>{
-        console.log(err.message);
+        if(response.status!==200){
+            const error = await response.json();
+            console.error("Error response ", error);
+            throw Error(error.message)
+        }
+        response = await response.json();
+        console.log("Response from HTTP request", response);
+        res.json({
+            success: true,
+            data: response
+        });
+    }
+    catch (error) {
+        console.log(error.message);
         res.status(400).json({
             success: false,
-            message: err.message
-        })
-    });
+            message: error.message
+        });
+    }
+    // .then(response=>{
+    //     if(response.status!==200){
+    //         console.log(response.json());
+    //         throw Error(response.json())
+    //     }
+    //     return response.json();
+    // })
+    // .then(body=>{
+    //     return res.json({
+    //         success: true,
+    //         data: body
+    //     });
+    // })
+    // .catch(err=>{
+    //     console.log(err.message);
+    //     res.status(400).json({
+    //         success: false,
+    //         message: err.message
+    //     });
+    // });
 
 }
 
