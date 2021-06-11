a26ceb21fbd6a1464e44d24a68f56134db537a35
cgnnanna <cgnnanna@gmail.com>
2021-06-11 12:44:34 +0100

    modified procfile file

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ app.use("/rates", rateRoutes);
 
 // app.listen(2000, ()=>{console.log("Server started on port 2000");})
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port, ()=>{console.log("Server started on port 8000");})
+let port = process.env.PORT || 8000;
+app.listen(port, ()=>{console.log(`Server started on port ${port}`);})
 
 
