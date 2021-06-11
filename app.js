7e368e07dfb3ea4885a8dc0536c96b7af2b88242
cgnnanna <cgnnanna@gmail.com>
2021-06-11 12:07:28 +0100

    modified app.js file

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express();
 app.use(express.json());
 app.use("/rates", rateRoutes);
 
-app.listen(2000, ()=>{console.log("Server started on port 2000");})
+// app.listen(2000, ()=>{console.log("Server started on port 2000");})
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port);
+app.listen(port, ()=>{console.log("Server started on port 8000");})
 
 
