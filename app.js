b598ad1b7f47430d7da8f772621c4909c4d99151
cgnnanna <cgnnanna@gmail.com>
2021-06-11 11:30:19 +0100

    added a Procfile file

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ app.use("/rates", rateRoutes);
 
 app.listen(2000, ()=>{console.log("Server started on port 2000");})
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
 
