de9982196961f6661413222547ccbceff06105a7
cgnnanna <cgnnanna@gmail.com>
2021-06-18 15:00:46 +0100

    added a middleware to handle requests to routes that are not accessible

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const app = express();
 app.use(express.json());
 app.use("/rates", rateRoutes);
 
+app.use((req, res) => {  
+    res.status(404).json({
+         message: "The route that you want to access does not exist"
+       });
+})
+
+
 // app.listen(2000, ()=>{console.log("Server started on port 2000");})
 
 let port = process.env.PORT || 8000;
-app.listen(port, ()=>{console.log(`Server started on port ${port}`);})
+app.listen(port, () => { console.log(`Server started on port ${port}`); })
 
 
